76d3b2f8285f5b087b105547f1867ba13d4e9ab2
Jack Arthur Harrhy <me@jackharrhy.com>
2025-10-13 19:01:26 -0230

    BIG bump

--- app/assets/map.tsx
+++ app/assets/map.tsx
@@ -2,18 +2,26 @@ import { type Remix, hydrated, connect } from "@remix-run/dom";
 import maplibregl from "maplibre-gl";
 
 import { routes } from "../../routes.ts";
-import { doc, dom } from "@remix-run/events";
+import { press } from "@remix-run/events/press";
 
 export const Map = hydrated(
   routes.assets.href({ path: "map.js#Map" }),
   function (this: Remix.Handle) {
     let map: maplibregl.Map | null = null;
 
+    console.log("in the component scope");
+
+    if (globalThis.window) {
+      window.update = this.update;
+    }
+
     return () => {
+      console.log("in the render scope");
       return (
         <div
           on={[
             connect((event) => {
+              console.log("in the connect scope");
               map = new maplibregl.Map({
                 container: event.currentTarget,
                 style: "https://demotiles.maplibre.org/globe.json",
@@ -22,6 +30,9 @@ export const Map = hydrated(
               });
 
               console.log("map created", map);
+            }),
+            press(() => {
+              console.log("clicked");
             }),
           ]}
         >
--- app/home.tsx
+++ app/home.tsx
@@ -5,12 +5,14 @@ import { routes } from "../routes.ts";
 import { Layout } from "./layout.tsx";
 import { render } from "./utils/render.ts";
 import { Map } from "./assets/map.tsx";
+import { Example } from "./assets/Example.tsx";
 
 export let home: InferRouteHandler<typeof routes.home> = () => {
   return render(
     <Layout>
       <div>welcome to remix v3 maplibre</div>
       <Map />
+      <Example />
     </Layout>
   );
 };
