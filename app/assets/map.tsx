855506e73780254641b4c7d7ca86364555476f66
Jack Arthur Harrhy <me@jackharrhy.com>
2025-10-13 18:45:51 -0230

    setting up maplibre, not working yet

--- app/assets/map.tsx
+++ app/assets/map.tsx
@@ -1,12 +1,33 @@
-import { type Remix, hydrated } from "@remix-run/dom";
+import { type Remix, hydrated, connect } from "@remix-run/dom";
+import maplibregl from "maplibre-gl";
 
 import { routes } from "../../routes.ts";
+import { doc, dom } from "@remix-run/events";
 
 export const Map = hydrated(
   routes.assets.href({ path: "map.js#Map" }),
   function (this: Remix.Handle) {
+    let map: maplibregl.Map | null = null;
+
     return () => {
-      return <div>map component</div>;
+      return (
+        <div
+          on={[
+            connect((event) => {
+              map = new maplibregl.Map({
+                container: event.currentTarget,
+                style: "https://demotiles.maplibre.org/globe.json",
+                center: [0, 0],
+                zoom: 1,
+              });
+
+              console.log("map created", map);
+            }),
+          ]}
+        >
+          map component
+        </div>
+      );
     };
   }
 );
